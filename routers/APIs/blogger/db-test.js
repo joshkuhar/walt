f21f845e320fcd84d6c24aaebb9367dac42f06ea
Josh Kuhar <jdkuhar@gmail.com>
2016-12-29 16:05:36 -0500

    tested db endpoints and schemas with test file

---
+++ routers/APIs/blogger/db-test.js
@@ -0,0 +1,67 @@
+var testRouter = require('express').Router();
+var Test = require('./db-testModel');
+
+
+testRouter.post('/r', function(req, res) {
+	console.log(req.body.name);
+	Test.create({
+		test: req.body.name
+	}, function(err, test) {
+		if (err) {
+			console.log(err);
+			return res.status(500).json({
+				message: 'Internal Server Error'
+				});
+			} 
+			res.status(200).json(test);
+		});   
+});
+
+testRouter.get('/r/:test', function(req, res){
+	Test.findOne({
+		test: req.params.test
+	}, function(err, test){
+		if(err) {
+			console.log(err);
+			return res.status(500).json({
+				message: 'Internal Server Error'
+			});
+		}
+		res.status(200).json(test);
+	});
+});
+
+module.exports = testRouter;
+/*
+var categoryRouter = require('express').Router();
+var Category = require('./categoryModel');
+
+
+categoryRouter.post('/category', function(req, res) {
+		console.log(req.body.name);
+		Category.create({
+			name: req.body.name
+		}, function(err, items) {
+			if (err) {
+				console.log(err);
+				return res.status(500).json({
+					message: 'Internal Server Error'
+					});
+				} 
+				res.status(200).json(items);
+			});   
+    });
+
+categoryRouter.get('/category/:name', function(req, res){
+	Category.findOne({
+		name: req.params.name
+	})
+	.populate('items')
+	.exec(function(err, cat){
+		if (err) {
+			console.log(err);
+		}
+		res.json(cat);
+	});
+});
+*/

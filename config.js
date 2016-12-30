0c47352cacaf9235e9258edb05b2f2ab8dc897db
Josh Kuhar <jdkuhar@gmail.com>
2016-12-30 16:34:45 -0500

    refactored and deleted files

--- config.js
+++
@@ -1,6 +0,0 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/blog' :
-                            'mongodb://localhost/blog-dev');
-exports.PORT = process.env.PORT || 8080;

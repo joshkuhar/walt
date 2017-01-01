fc9ad286183ce85047d656b266c9be43d1f843c0
Josh Kuhar <jdkuhar@gmail.com>
2016-12-31 19:45:40 -0500

    changed mock data

--- js/mock-data.js
+++ js/mock-data.js
@@ -1,41 +1,42 @@
 var Data = {
-	type: "Blog Post",
-	items: [
-		{
-			_id: 0,
-			title: "Oberon",
-			category: "drama",
-			date: 'Tue Dec 19 2016 20:20:35 GMT-0500 (EST)',
-			content: "I am that merry wanderer of the night. I jest to Oberon and make him smile When I a fat and bean-fed horse beguile, Neighing in likeness of a filly foal: And sometime lurk I in a gossip’s bowl, In very likeness of a roasted crab, And when she drinks, against her lips I bob And on her wither’d dewlap pour the ale. \n\nThe wisest aunt, telling the saddest tale, Sometime for three-foot stool mistaketh me; Then slip I from her bum, down topples she, And ‘tailor’ cries, and falls into a cough; And then the whole quire hold their hips and laugh, And waxen in their mirth and neeze and swear A merrier hour was never wasted there."
-		},
-		{
-			_id: 2,
-			title: "Hermia",
-			category: "comedy",
-			date: 'Tue Dec 20 2016 20:20:35 GMT-0500 (EST)',
-			content: "I am that merry wanderer of the night. I jest to Oberon and make him smile When I a fat and bean-fed horse beguile, Neighing in likeness of a filly foal: And sometime lurk I in a gossip’s bowl, In very likeness of a roasted crab \n\nAnd when she drinks, against her lips I bob And on her wither’d dewlap pour the ale. The wisest aunt, telling the saddest tale, Sometime for three-foot stool mistaketh me; Then slip I from her bum, down topples she, And ‘tailor’ cries, and falls into a cough; And then the whole quire hold their hips and laugh, And waxen in their mirth and neeze and swear A merrier hour was never wasted there."
-		},
-		{
-			_id: 3,
-			title: "Demetrius",
-			category: "drama",
-			date: "Fri Dec 17 2016 20:20:35 GMT-0500 (EST)",
-			content: "I pray thee, give it me. I know a bank where the wild thyme blows, Where oxlips and the nodding violet grows, Quite over-canopied with luscious woodbine, With sweet musk-roses and with eglantine: There sleeps Titania sometime of the night, Lull'd in these flowers with dances and delight; And there the snake throws her enamell'd skin, Weed wide enough to wrap a fairy in: And with the juice of this I'll streak her eyes, And make her full of hateful fantasies. \n\nTake thou some of it, and seek through this grove: A sweet Athenian lady is in love With a disdainful youth: anoint his eyes; But do it when the next thing he espies May be the lady: thou shalt know the man By the Athenian garments he hath on. Effect it with some care, that he may prove More fond on her than she upon her love: And look thou meet me ere the first cock crow."
-		}
-	],
+
 	categories: [
 		{
-			category: "economy",
-			_id: "11"
+			category: "Philosophy"
 		},
 		{
-			category: "tribe life",
-			_id: "22"
+			category: "Old Friends"
 		},
 		{
-			category: "furry companions",
-			_id: "33"
+			category: "Long Talks"
+		},
+		{
+			category: "Serious"
+		},
+		{
+			category: "Not Serious"
+		},
+		{
+			category: "Chick Lit"
+		},
+		{
+			category: "Travel"
+		},
+		{
+			category: "Poetry"
 		}
+		// {
+		// 	category: "  -----  "
+		// },
+		// {
+		// 	category: "economy"
+		// },
+		// {
+		// 	category: "tribe life"
+		// },
+		// {
+		// 	category: "furry companions"
+		// },
 		// {
 		// 	category: "nature"
 		// },
@@ -67,8 +68,6 @@ var Data = {
 		// 	category: "digital paraphernalia"
 		// }	
 	],
-	aboutMe: "I must confess that I have heard so much, And with Demetrius thought to have spoke thereof; But, being over-full of self-affairs, My mind did lose it. But, Demetrius, come; And come, Egeus; you shall go with me, I have some private schooling for you both. For you, fair Hermia, look you arm yourself To fit your fancies to your father's will; Or else the law of Athens yields you up-- Which by no means we may extenuate-- To death, or to a vow of single life. \n\nCome, my Hippolyta: what cheer, my love? Demetrius and Egeus, go along: I must employ you in some business Against our nuptial and confer with you Of something nearly that concerns yourselves."
-
 };
 
 module.exports = Data;

f54a7976003e1a5bd1b3981b51f6402bee90ad27
Josh Kuhar <jdkuhar@gmail.com>
2017-01-01 21:20:10 -0500

    created make believe data with ipsum smipsum dizzle

---
+++ js/mock-data-loader.js
@@ -0,0 +1,33 @@
+
+var categories = [
+	{ "_id" : "5869b745b85bc0532e4c92f8"},
+	{ "_id" : "5869b745b85bc0532e4c92f6"},
+	{ "_id" : "5869b745b85bc0532e4c92f7"}
+];
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var dates = ["1", "3", "7", "8", "11", "12", "14", "15", "26", "29"];
+var years = ["2015", "2016"];
+
+var titles = ["That time when", "The other day", "Life", "When is it okay?", "If I had a dollar for every", "Looking on the bright side of things", "Lorem ipsum dolor sit amet"];
+var content = ["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem nam nesciunt vitae unde accusamus. Asperiores sapiente laborum accusamus commodi tenetur at quam, atque omnis labore vero nulla maiores expedita. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore deleniti unde cum debitis sunt corrupti pariatur nihil expedita error, alias nulla illo nostrum, ipsa assumenda aliquid earum exercitationem tempore minus.ipsum dolor sit amet, consectetur adipisicing elit. Inventore deleniti unde cum debitis sunt corrupti pariatur nihil expedita error, alias nulla illo nostrum, ipsa assumenda aliquid earum exercitationem tempore minus.ipsum dolor sit amet, consectetur adipisicing elit. Inventore deleniti unde cum debitis sunt corrupti pariatur nihil expedita error, alias nulla illo nostrum, ipsa assumenda aliquid earum exercitationem tempore minus.\n\nipsum dolor sit amet, consectetur adipisicing elit. Inventore deleniti unde cum debitis sunt corrupti pariatur nihil expedita error, alias nulla illo nostrum, ipsa assumenda aliquid earum exercitationem tempore minus.ipsum dolor sit amet, consectetur adipisicing elit. Inventore deleniti unde cum debitis sunt corrupti pariatur nihil expedita error, alias nulla illo nostrum, ipsa assumenda aliquid earum exercitationem tempore minus.ipsum dolor sit amet, consectetur adipisicing elit. Inventore deleniti unde cum debitis sunt corrupti pariatur nihil expedita error, alias nulla illo nostrum, ipsa assumenda aliquid earum exercitationem tempore minus.", "Inventore deleniti unde cum debitis sunt corrupti pariatur nihil expedita error, alias nulla illo nostrum, ipsa assumenda aliquid earum exercitationem tempore minus.ipsum dolor sit amet, consectetur adipisicing elit. Inventore deleniti unde cum debitis sunt corrupti pariatur nihil expedita error, alias nulla illo nostrum, ipsa assumenda aliquid earum exercitationem tempore minus.ipsum dolor sit amet, consectetur adipisicing elit. Inventore deleniti unde cum debitis sunt corrupti pariatur nihil expedita error, alias nulla illo nostrum, ipsa assumenda aliquid earum exercitationem tempore minus.ipsum dolor sit amet, consectetur adipisicing elit.", "Inventore deleniti \n\nNde cum debitis sunt corrupti pariatur nihil expedita error, alias nulla illo nostrum, ipsa assumenda aliquid earum exercitationem tempore minus.ipsum dolor sit amet, consectetur adipisicing elit. Inventore deleniti unde cum debitis sunt corrupti pariatur nihil expedita error, alias nulla illo nostrum, ipsa assumenda aliquid earum exercitationem tempore minus.ipsum dolor sit amet, consectetur adipisicing elit.\n\nInventore deleniti unde cum debitis sunt corrupti pariatur nihil expedita error, alias nulla illo nostrum, ipsa assumenda aliquid earum exercitationem tempore minus.ipsum dolor sit amet, consectetur adipisicing elit. \n\nInventore deleniti unde cum debitis sunt corrupti pariatur nihil expedita error, alias nulla illo nostrum, ipsa assumenda aliquid earum exercitationem tempore minus.ipsum dolor sit amet, consectetur adipisicing elit."]
+
+var blogPost = function(categories, months, dates, years, titles, content){
+	var blogArray = [];
+	for (var index = 0; index < months.length*2; index++) {
+		var year = index < months.length ? 0 : 1;
+		blogArray.push({
+			title: titles[index%titles.length],
+			content: content[index%content.length],
+			month: months[index%months.length],
+			date: dates[index%dates.length],
+			year: years[year],
+			categoryId: categories[index%categories.length]._id
+		})
+	}
+	return blogArray;
+}
+
+var data = blogPost(categories, months, dates, years, titles, content);
+
+module.exports = data;

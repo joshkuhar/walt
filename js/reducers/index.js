ed6358ec0d527abfe08a05d958c1cc24f592fe73
Josh Kuhar <jdkuhar@gmail.com>
2017-01-04 18:35:04 -0500

    refactored with Object.assign

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,154 +1,88 @@
 var actions = require('../actions/index');
-var Data = require('../mock-data');
+var Categories = require('../mock-categories');
 var DummyBlogs = require('../mock-data-loader');
 
 var initialState = {
 	about: "",
-	test: "I'm the test and I worked",
 	title: "",
 	blog: "",
 	content: "",
 	category: "",
-	categories: Data,
+	categoriesForLoading: Categories.categoriesForLoading,
+	categories: Categories.categories,
 	dummyBlogs: DummyBlogs,
 	blogs: [{_id: "111", blogPost: {title: "foo", month: "Jan", date: "1", year: "2016"}}]
-	//blog: state.blogs[0].blogPost.title
-		
-	
 };
 
 var blogReducer = function(state, action) {
 	state = state || initialState;
 	if (action.type === actions.UPDATE_BLOG) {
-		return {
-			title: state.title,
-			category: state.category,
-			categories: state.categories,
-			blog: action.blog,
-			blogs: state.blogs,
-			about: state.about
-		}
+		return 	Object.assign({}, state, {
+			blog: action.blog
+		})
 	}
 	else if (action.type === actions.LOAD_SUCCESS) {
-		return {
-			category: state.category,
+		return Object.assign({}, state, {
 			categories: action.categories
-		}
+		})
 	}
 	else if (action.type === actions.BLOG_LOAD_SUCCESS) {
-		return {
+		return Object.assign({}, state, {
 			blogs: action.blogs
-		}
+		})
 	}
 	else if (action.type === actions.GET_CATEGORIES_SUCCESS){
-		return {
-			title: state.title,
-			blog: state.blog,
-			category: state.category,
+		return Object.assign({}, state, {
 			categories: action.categories
-		}
+		})
 	}
 	else if (action.type === actions.UPDATE_TITLE) {
-		return {
-			title: action.title,
-			blog: state.blog,
-			blogs: state.blogs,
-			about: state.about,
-			category: state.category,
-			categories: state.categories
-		}
+		return Object.assign({}, state, {
+			title: action.title
+		})
 	}
 	else if (action.type === actions.SELECT_CATEGORY) {
-		return {
-			category: action.category,
-			categories: state.categories,
-			title: state.title,
-			blog: state.blog,
-			about: state.about,
-			blogs: state.blogs
-		}
+		return Object.assign({}, state, {
+			category: action.category
+		})
 	}
 	else if (action.type === actions.SET_BLOG_ENTRY_FORM) {
-		return {
-			category: state.category,
-			categories: state.categories,
+		return Object.assign({}, state, {
 			title: action.title,
-			blog: action.blog,
-			about: state.about,
-			blogs: state.blogs
-		}
+			blog: action.blog
+		})
 	}
 	else if (action.type === actions.POST_BLOG_SUCCESS) {
-		return {
-			category: state.category,
-			categories: state.categories,
-			title: state.title,
-			blog: action.data.blogPost.content,
-			success: "post success!",
-			about: state.about
-		}
+		return Object.assign({}, state, {
+			blog: action.data.blogPost.content
+		})
 	}
 	else if(action.type === actions.GET_BLOG_TO_EDIT) {
-		return {
-			about: state.about,
-			blogs: state.blogs,
+		return Object.assign({}, state, {
 			title: action.blog.blogPost.title,
-			blog: action.blog.blogPost.content,
-			category: state.category,
-			categories: state.categories
-		}
+			blog: action.blog.blogPost.content
+		})
 	}
 	else if(action.type === actions.PUT_BLOG_SUCCESS) {
-		return {
-			category: state.category,
-			categories: state.categories,
-			title: state.title,
-			blog: state.blog,
-			success: "post success!",
-			about: state.about,
-			blogs: state.blogs
-		}
+		return state
 	}
 	else if(action.type === actions.GET_BLOGS_SUCCESS) {
-		return {
-			about: state.about,
-			blogs: action.blogs,
-			title: state.title,
-			blog: state.blog,
-			category: state.category,
-			categories: state.categories
-		}
+		return Object.assign({}, state, {
+			blogs: action.blogs
+		})
 	}
 	else if(action.type === actions.GET_ABOUT_SUCCESS) {
-		return {
-			about: action.about,
-			category: state.category,
-			categories: state.categories,
-			title: state.title,
-			blog: state.blog,
-			blogs: state.blogs
-		}
+		return Object.assign({}, state, {
+			about: action.about
+		})
 	}
 	else if(action.type === actions.CHANGE_ABOUT) {
-		return {
-			about: action.about,
-			blogs: state.blogs,
-			title: state.title,
-			blog: state.blog,
-			category: state.category,
-			categories: state.categories
-		}
+		return Object.assign({}, state, {
+			about: action.about
+		})
 	}
 	else if(action.type === actions.UPDATE_ABOUT_SUCCESS) {
-		return {
-			about: state.about,
-			blogs: state.blogs,
-			title: state.title,
-			blog: state.blog,
-			category: state.category,
-			categories: state.categories,
-			success: "update success!"
-		}
+		return state
 	}
 
 	return state;

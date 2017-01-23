b762eb38007f8ed815209908c99b4bcf8543ee38
Josh Kuhar <jdkuhar@gmail.com>
2017-01-23 18:58:02 -0500

    updated

--- js/components/dashboard-navigator.js
+++ js/components/dashboard-navigator.js
@@ -29,12 +29,14 @@ var DashboardNavBar  = React.createClass({
 				<Link to="/">Home Page</Link>
 				<button onClick={this.onStore}>store</button>
 				<div onClick={this.handleSignOut}className="sign-out">Sign Out</div>
-				<h2 className="dashboard-header">Dashboard</h2>
-				<div className="dashboard-nav-bar">
-					<div className="dashboard-nav-bar-item-wrapper"><Link to="/dashboard/edit"><div className="dashboard-nav-bar-item">Blog List</div></Link></div>
-					<div className="dashboard-nav-bar-item-wrapper"><Link to="/dashboard/create"><div className="dashboard-nav-bar-item">Write New Blog</div></Link></div>
-					<div className="dashboard-nav-bar-item-wrapper"><Link to="/dashboard/about"><div className="dashboard-nav-bar-item">Edit About</div></Link></div>
-					<div className="dashboard-nav-bar-item-wrapper"><Link to="/dashboard/category"><div className="dashboard-nav-bar-item">Categories</div></Link></div>
+				<div className="dashboard-top-navigator">wtf
+					<h2 className="dashboard-header">Dashboard</h2>
+					<div className="dashboard-nav-bar">
+						<div className="dashboard-nav-bar-item-wrapper"><Link to="/dashboard/edit"><div className="dashboard-nav-bar-item">Blog List</div></Link></div>
+						<div className="dashboard-nav-bar-item-wrapper"><Link to="/dashboard/create"><div className="dashboard-nav-bar-item">Write New Blog</div></Link></div>
+						<div className="dashboard-nav-bar-item-wrapper"><Link to="/dashboard/about"><div className="dashboard-nav-bar-item">Edit About</div></Link></div>
+						<div className="dashboard-nav-bar-item-wrapper"><Link to="/dashboard/category"><div className="dashboard-nav-bar-item">Categories</div></Link></div>
+					</div>
 				</div>
 				{this.props.children}
 			</div>

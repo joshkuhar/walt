ec3b4f308c281b922adda81c0234872fe71041b5
Josh Kuhar <jdkuhar@gmail.com>
2017-01-27 16:34:42 -0500

    combined components

--- js/components/about/about.js
+++ js/components/about/about.js
@@ -4,7 +4,7 @@ var ReactDOM = require('react-dom');
 var About = function(props) {
 	return (
 			<div className="about">
-				<div className="about-name">i am Puck</div>
+				<div className="about-name">Puck</div>
 				<div className="about-line"></div>
 				<div className="about-title">experienced woodland spirit</div>
 				<div className="about-text">{props.about}</div>
